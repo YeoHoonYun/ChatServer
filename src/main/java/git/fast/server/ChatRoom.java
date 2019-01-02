390d4342540c4430b706b3c58bfa5a2f22a09dc5
dabinchoi <dabinnn1011@naver.com>
2019-01-02 19:46:36 +0900

    chatRoom finish

--- src/main/java/git/fast/server/ChatRoom.java
+++ src/main/java/git/fast/server/ChatRoom.java
@@ -1,5 +1,7 @@
 package git.fast.server;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ChatRoom {
@@ -7,10 +9,34 @@ public class ChatRoom {
     private String title;
     private List<ChatUser> chatUsers;
 
-        public ChatRoom(ChatUser chatUser,String title){
+    public ChatRoom(ChatUser chatUser, String title) {
+        this.title = title;
+        chatUsers = Collections.synchronizedList(new ArrayList<ChatUser>());
+        chatUsers.add(chatUser);
 
+    }
 
+    public boolean existsUser(ChatUser chatUser) {
+        return chatUsers.contains(chatUser);
+    }
 
-        }
+    public String getTitle(){
+        return title;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public List<ChatUser> getChatUsers(){
+        return chatUsers;
+
+    }
+
+    public void setChatUsers(List<ChatUser> chatUsers) {
+        this.chatUsers = chatUsers;
+    }
+    public void addChatUser(ChatUser chatUser){
+        chatUsers.add(chatUser);
+    }
 
 }
