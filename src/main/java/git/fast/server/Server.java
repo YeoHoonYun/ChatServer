a97d2432c1cb83d7b6b10b86f90b4068db5e5dc7
yun <cjswo9207@gmail.com>
2019-01-04 15:27:36 +0900

    no message

--- src/main/java/git/fast/server/Server.java
+++ src/main/java/git/fast/server/Server.java
@@ -1,9 +1,5 @@
 package git.fast.server;
 
-/**
- * Created by cjswo9207u@gmail.com on 2019-01-02
- * Github : https://github.com/YeoHoonYun
- */
 public class Server {
     public static void main(String[] args){
         ChatServer chatServer = new ChatServer(8000);
