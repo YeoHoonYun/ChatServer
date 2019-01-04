3753d1ff8626a6598c231b853cceda23e252d471
choiwono <treasure_b@naver.com>
2019-01-04 12:01:28 +0900

    #1 코드리뷰후 - 수정

--- src/main/java/git/fast/server/ChatHouse.java
+++ src/main/java/git/fast/server/ChatHouse.java
@@ -9,30 +9,30 @@ public class ChatHouse {
     List<ChatRoom> rooms;
 
     public ChatHouse(){
-        lobby = Collections.synchronizedList(new ArrayList<>());
-        rooms = Collections.synchronizedList(new ArrayList<>());
+        lobby = new ArrayList<>();
+        rooms = new ArrayList<>();
     }
 
-    public void addLobby(ChatUser chatUser){
+    public synchronized void addLobby(ChatUser chatUser){
         lobby.add(chatUser);
     }
 
-    public List<ChatUser> getLobby(){   return lobby; }
+    public synchronized List<ChatUser> getLobby(){   return lobby; }
 
-    public void createRoom(ChatUser chatUser, String title){
+    public synchronized void createRoom(ChatUser chatUser, String title){
         rooms.add(new ChatRoom(chatUser, title));
         chatUser.setisRoom(true);
         chatUser.setIsAdmin(true);
         exitLobby(chatUser);
     }
 
-    public void joinRoom(ChatUser chatUser , int index){
+    public synchronized void joinRoom(ChatUser chatUser , int index){
         rooms.get(index).addChatUser(chatUser);
         chatUser.setisRoom(true);
         exitLobby(chatUser);
     }
 
-    public void exitRoom(ChatUser chatUser){
+    public synchronized void exitRoom(ChatUser chatUser){
         for(ChatRoom cr : rooms){
             if(cr.existsUser(chatUser)){
                 cr.getChatUsers().remove(chatUser);
@@ -53,11 +53,11 @@ public class ChatHouse {
     }
 
 
-    public void exitLobby(ChatUser chatUser){
+    public synchronized void exitLobby(ChatUser chatUser){
         lobby.remove(chatUser);
     }
 
-    public List<ChatUser> getChatUsers(ChatUser chatUser){
+    public synchronized List<ChatUser> getChatUsers(ChatUser chatUser){
         for(ChatRoom cr : rooms){
             if(cr.existsUser(chatUser)){
                 return cr.getChatUsers();
@@ -66,7 +66,7 @@ public class ChatHouse {
         return new ArrayList<ChatUser>();
     }
 
-    public ChatRoom getChatRoom(ChatUser chatUser){
+    public synchronized ChatRoom getChatRoom(ChatUser chatUser){
         for(ChatRoom cr : rooms){
             if(cr.existsUser(chatUser)){
                 return cr;
@@ -75,15 +75,15 @@ public class ChatHouse {
         return null;
     }
 
-    public List<ChatRoom> getRooms(){
+    public synchronized List<ChatRoom> getRooms(){
         return rooms;
     }
 
-    public void deleteRoom(ChatRoom chatRoom){
+    public synchronized void deleteRoom(ChatRoom chatRoom){
         rooms.remove(chatRoom);
     }
 
-    public ChatUser roomCreater(ChatRoom chatRoom){
+    public synchronized ChatUser roomCreater(ChatRoom chatRoom){
         for(ChatUser chatUser : chatRoom.getChatUsers()){
             if(chatUser.isAdmin()){
                 return chatUser;
@@ -91,7 +91,37 @@ public class ChatHouse {
         }
         return null;
     }
-    public void chatTwo(ChatUser to, String text){
+    public synchronized void chatTwo(ChatUser to, String text){
         to.write("200///"+text);
+    }
+
+    public void printHelp(int option, ChatUser chatUser){
+        switch(option) {
+            case 0 : chatUser.write("======== 메뉴(방장) =========");
+                     chatUser.write(" /exit   : 방나가기");
+                     chatUser.write(" /kick   : 유저 강퇴");
+                     chatUser.write(" /help   : 도움말");
+                     chatUser.write(" /talk : 귓속말");
+                     chatUser.write(" /visi 로비유저번호 : 초대하기");
+                     chatUser.write("============================");
+                     break;
+
+            case 1 : chatUser.write("============ 메뉴 ===========");
+                     chatUser.write(" /exit   : 방나가기");
+                     chatUser.write(" /help   : 도움말");
+                     chatUser.write(" /talk : 귓속말");
+                     chatUser.write("============================");
+                     break;
+        }
+    }
+
+    public void printChatUsers(ChatUser chatUser) {
+        int index = 0;
+        for (ChatUser cu : getChatUsers(chatUser)) {
+            if (index != 0) {
+                chatUser.write(index + " | 이름 : " + cu.getName());
+            }
+            index++;
+        }
     }
 }
