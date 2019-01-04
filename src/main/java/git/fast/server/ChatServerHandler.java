4fdc0ad24d3dc5a06f80f37dbe5fc0adc6b6b2ff
yun <cjswo9207@gmail.com>
2019-01-04 15:48:11 +0900

    docs : if들과 관련된 코드들 메서드로 변환

--- src/main/java/git/fast/server/ChatServerHandler.java
+++ src/main/java/git/fast/server/ChatServerHandler.java
@@ -40,65 +40,9 @@ class ChatServerHandler extends Thread {
                     } else if (chatUser.isAdmin() && msg.indexOf("/kick") == 0) {
                         if (kick()) continue;
                     } else if (chatUser.isAdmin() && msg.indexOf("/visi") == 0) {
-                        // 방장이 로비에 있는 인원 초대할 때
-                        try {
-                            chatUser.write("============로비에 있는 유저정보===============");
-                            int num = 0;
-                            if (chatHouse.getLobby().size() != 0) {
-                                for (ChatUser cr : chatHouse.getLobby()) {
-                                    if (cr == chatUser) {
-//                                        chatUser.write(num + " : " + cr.getName() + "(나)");
-//                                        num++;
-                                        continue;
-                                    } else {
-                                        chatUser.write(num + " : " + cr.getName());
-                                    }
-                                    num++;
-                                }
-                            }
-                            try {
-                                chatUser.write("300///초대할 유저 번호를 입력하세요.");
-                                int index = Integer.parseInt(chatUser.read());
-                                ChatUser visiUser = chatHouse.getLobby().get(index);
-                                chatHouse.getLobby().remove(visiUser);
-                                chatHouse.getChatRoom(chatUser).addChatUser(visiUser);
-                                visiUser.setisRoom(true);
-                                chatUser.write(visiUser.getName() + "님을 초대하였습니다..");
-                                visiUser.write(chatUser.getName() + "님이 초대하였습니다..");
-
-                                List<ChatUser> userList = chatHouse.getChatUsers(chatUser);
-                                for (ChatUser cu : userList) {
-                                    cu.write("300///"+visiUser.getName() + "님이 방에 입장했습니다.");
-                                }
-                            }catch (Exception e){
-                                System.out.println("300///유저 번호가 없거나 잘못된 형식입니다.");
-                            }
-                        } catch (Exception e){
-
-                            chatUser.write("300///유저번호를 입력하세요.");
-                        }
+                        visiRoom();
                     } else if (msg.indexOf("/talk") == 0) {
-                        int index = 0;
-                        if (chatHouse.getChatUsers(chatUser).size() != 0) {
-                            for (ChatUser cr : chatHouse.getChatUsers(chatUser)) {
-                                if(cr == chatUser) {
-//                                    chatUser.write(index + " : " + cr.getName()+"(나)");
-                                    index++;
-                                }else{
-                                    chatUser.write(index + " : " + cr.getName());
-                                    index++;
-                                }
-                            }
-                        }
-                        try {
-                            chatUser.write("300///입력하고자 하는 유저번호를 넣으세요.");
-                            ChatUser inUser = chatHouse.getChatRoom(chatUser).getChatUsers().get(Integer.parseInt(chatUser.read()));
-                            chatUser.write("전달할 메시지를 적어주세요.");
-                            String string = chatUser.read();
-                            chatHouse.chatTwo(inUser, chatUser.getName() + " : " + string);
-                        }catch (Exception e){
-                            chatUser.write("유저 번호가 없거나 잘못된 형식입니다.");
-                        }
+                        talkRoom();
                     } else {
                         List<ChatUser> userList = chatHouse.getChatUsers(chatUser);
                         for (ChatUser cu : userList) {
@@ -144,36 +88,9 @@ class ChatServerHandler extends Thread {
                             chatUser.close();
                         }
                     } else if (msg.indexOf("/talk") == 0) {
-                        int index = 0;
-                        if (chatHouse.getLobby().size() != 0) {
-                            for (ChatUser cr : chatHouse.getLobby()) {
-                                if (cr == chatUser) {
-//                                    chatUser.write(index + " : " + cr.getName() + "(나)");
-                                    index++;
-                                } else {
-                                    chatUser.write(index + " : " + cr.getName());
-                                    index++;
-                                }
-
-                            }
-                        }
-                        try {
-                            chatUser.write("300///입력하고자 하는 유저번호를 넣으세요.");
-                            ChatUser inUser = chatHouse.getLobby().get(Integer.parseInt(chatUser.read()));
-                            chatUser.write("전달할 메시지를 적어주세요.");
-                            String string = chatUser.read();
-                            chatHouse.chatTwo(inUser, chatUser.getName() + " : " + string);
-                        }catch (Exception e){
-                            chatUser.write("유저 번호가 없거나 잘못된 형식입니다.");
-                        }
+                        talkLobby();
                     } else if (msg.indexOf("/help") == 0) {
-                        chatUser.write("============ 메뉴 ===========");
-                        chatUser.write("/create [방이름]   : 방을 생성합니다.");
-                        chatUser.write("/list            : 방 리스트를 입력하세요. ");
-                        chatUser.write("/join [방번호]     : 방번호를 입력하세요.");
-                        chatUser.write("/quit            : 종료합니다.");
-                        chatUser.write("/talk : 귓속말");
-                        chatUser.write("=============================");
+                        boardLobby();
                     } else {
                         List<ChatUser> lobbyList = chatHouse.getLobby();
                         for (ChatUser cu : lobbyList) {
@@ -189,6 +106,101 @@ class ChatServerHandler extends Thread {
             System.out.println(chatUser.getName() + "연결 종료");
         }
 
+    }
+
+    public void boardLobby() {
+        chatUser.write("============ 메뉴 ===========");
+        chatUser.write("/create [방이름]   : 방을 생성합니다.");
+        chatUser.write("/list            : 방 리스트를 입력하세요. ");
+        chatUser.write("/join [방번호]     : 방번호를 입력하세요.");
+        chatUser.write("/quit            : 종료합니다.");
+        chatUser.write("/talk : 귓속말");
+        chatUser.write("=============================");
+    }
+
+    public void talkLobby() {
+        int index = 0;
+        if (chatHouse.getLobby().size() != 0) {
+            for (ChatUser cr : chatHouse.getLobby()) {
+                if (cr == chatUser) {
+//                                    chatUser.write(index + " : " + cr.getName() + "(나)");
+                    index++;
+                } else {
+                    chatUser.write(index + " : " + cr.getName());
+                    index++;
+                }
+
+            }
+        }
+        try {
+            chatUser.write("300///입력하고자 하는 유저번호를 넣으세요.");
+            ChatUser inUser = chatHouse.getLobby().get(Integer.parseInt(chatUser.read()));
+            chatUser.write("전달할 메시지를 적어주세요.");
+            String string = chatUser.read();
+            chatHouse.chatTwo(inUser, chatUser.getName() + " : " + string);
+        }catch (Exception e){
+            chatUser.write("유저 번호가 없거나 잘못된 형식입니다.");
+        }
+    }
+
+    public void talkRoom() {
+        int index = 0;
+        if (chatHouse.getChatUsers(chatUser).size() != 0) {
+            for (ChatUser cr : chatHouse.getChatUsers(chatUser)) {
+                if(cr == chatUser) {
+//                                    chatUser.write(index + " : " + cr.getName()+"(나)");
+                    index++;
+                }else{
+                    chatUser.write(index + " : " + cr.getName());
+                    index++;
+                }
+            }
+        }
+        try {
+            chatUser.write("300///입력하고자 하는 유저번호를 넣으세요.");
+            ChatUser inUser = chatHouse.getChatRoom(chatUser).getChatUsers().get(Integer.parseInt(chatUser.read()));
+            chatUser.write("전달할 메시지를 적어주세요.");
+            String string = chatUser.read();
+            chatHouse.chatTwo(inUser, chatUser.getName() + " : " + string);
+        }catch (Exception e){
+            chatUser.write("유저 번호가 없거나 잘못된 형식입니다.");
+        }
+    }
+
+    public void visiRoom() {
+        // 방장이 로비에 있는 인원 초대할 때
+        chatUser.write("============로비에 있는 유저정보===============");
+        int num = 0;
+        if (chatHouse.getLobby().size() != 0) {
+            for (ChatUser cr : chatHouse.getLobby()) {
+                if (cr == chatUser) {
+//                                        chatUser.write(num + " : " + cr.getName() + "(나)");
+//                                        num++;
+                    continue;
+                } else {
+                    chatUser.write(num + " : " + cr.getName());
+                }
+                num++;
+            }
+        }
+        try {
+                chatUser.write("300///초대할 유저 번호를 입력하세요.");
+                int index = Integer.parseInt(chatUser.read());
+                ChatUser visiUser = chatHouse.getLobby().get(index);
+                chatHouse.getLobby().remove(visiUser);
+                chatHouse.getChatRoom(chatUser).addChatUser(visiUser);
+                visiUser.setisRoom(true);
+                chatUser.write(visiUser.getName() + "님을 초대하였습니다..");
+                visiUser.write(chatUser.getName() + "님이 초대하였습니다..");
+
+                List<ChatUser> userList = chatHouse.getChatUsers(chatUser);
+                for (ChatUser cu : userList) {
+                    cu.write("300///"+visiUser.getName() + "님이 방에 입장했습니다.");
+                }
+        } catch (Exception e){
+
+            chatUser.write("300///유저 번호가 없거나 잘못된 형식입니다.");
+        }
     }
 
     private boolean kick() {
